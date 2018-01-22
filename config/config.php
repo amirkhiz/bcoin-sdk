cc1b8785c89dbe73a5088880f4778ec35a71c3cf
habil.crypto <habil.crypto@gmail.com>
2018-01-22 16:05:09 +0300

    Initial Project

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: habil.crypto
+ * Date: 22.01.2018
+ * Time: 15:50
+ */
+
+return [
+    'username' => 'Wallet-Username',
+    'password' => 'Wallet-Password',
+    'ip'       => 'Full-Node-IP',
+    'port'     => 18332,
+];
